3a09c61510f841ae79fc4d383b6d44cedc7f0004
AstralisLLC <161306414+AstralisLLC@users.noreply.github.com>
2024-05-04 01:29:00 -0700

    Fixed dark ores

--- xray.js
+++ xray.js
@@ -1,4 +1,4 @@
-// I stole this from radman, so it def isn't mine :sob:
+// Stolen code, not mine
 
 (function () {
     var enabled = false
@@ -16,26 +16,29 @@
     var targets = ["diamond_block","diamond_ore","gold_block","gold_ore","iron_block","iron_ore","coal_block","coal_ore","emerald_ore","obsidian","emerald_block","redstone_ore","redstone_block","lapis_ore","lapis_block","chest","furnace","lit_furnace","ender_chest"]; //The target blocks
     var allblocks = Object.keys(ModAPI.blocks); //List of all block IDs
     function update() {
-      ModAPI.displayToChat({msg: "xray Enabled!"})
-      allblocks.forEach(block=>{ //Loop through all the blocks
+        ModAPI.displayToChat({msg: "§5[§dXRAY§5] §bXRAY is now enabled!"})
+        allblocks.forEach(block=>{ //Loop through all the blocks
         if (targets.includes(block)) { //Render targets
-          ModAPI.blocks[block].forceRender = true;
-          ModAPI.blocks[block].reload(); //Update RenderEngine
-        } else if (ModAPI.blocks[block] && ("noRender" in ModAPI.blocks[block])) { //Checks if should render
-          ModAPI.blocks[block].noRender = true;
-          ModAPI.blocks[block].reload(); //Update RenderEngine
-        }
-      });
-      ModAPI.reloadchunks()
+                ModAPI.blocks[block].forceRender = true;
+                ModAPI.blocks[block].lightValue = 10;
+                ModAPI.blocks[block].reload(); //Update RenderEngine
+            } else if (ModAPI.blocks[block] && ("noRender" in ModAPI.blocks[block])) { //Checks if should render
+                ModAPI.blocks[block].noRender = true;
+                ModAPI.blocks[block].lightValue = 0;
+                ModAPI.blocks[block].reload(); //Update RenderEngine
+            }
+        });
+        ModAPI.reloadchunks()
     }
+    
     function disable(){
-      ModAPI.displayToChat({msg: "xray Disabled!"})
-              allblocks.forEach(block=>{ //Loop through all the blocks
- if (ModAPI.blocks[block] && ("noRender" in ModAPI.blocks[block])) { 
-          ModAPI.blocks[block].noRender = false;
-          ModAPI.blocks[block].reload(); //Update RenderEngine
-        }
-      });
-    ModAPI.reloadchunks()
+        ModAPI.displayToChat({msg: "§5[§dXRAY§5] §bXRAY is now disabled!"})
+        allblocks.forEach(block=>{ //Loop through all the blocks
+            if (ModAPI.blocks[block] && ("noRender" in ModAPI.blocks[block])) { 
+                ModAPI.blocks[block].noRender = false;
+                ModAPI.blocks[block].reload(); //Update RenderEngine
+            }
+        });
+        ModAPI.reloadchunks()
     }
   })();
